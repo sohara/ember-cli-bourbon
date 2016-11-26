f024928435e739f100396d7e4fb074b5abb4b055
Sean O'Hara <sohara@sohara.com>
2016-11-26 11:47:53 -0500

    Revert "Add in-repo addons support"
    This reverts commit 0c2e7682b798f20c7034913bae51bc1b276aeb8b.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 var path = require('path');
 var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-bourbon',
@@ -17,31 +16,6 @@ module.exports = {
       srcDir: '/assets/stylesheets',
       destDir: '/app/styles'
     });
-    var bourbonAddonTree = new Funnel(this.treeGenerator(bourbonPath), {
-      srcDir: '/assets/stylesheets',
-      destDir: '/addon/styles'
-    });
-    // return mergeTrees([bourbonTree, bourbonAddonTree], {
-    //   annotation: 'cli-bourbon merged'
-    // });
     return bourbonTree;
-  },
-
-  // treeForAddon: function() {
-  //   console.log("EMBER-CLI-BOURBON");
-  //   var tree = this._super.treeForAddon.apply(this, arguments);
-  //   // var checker = new VersionChecker(this);
-  //   // var isOldEmber = checker.for('ember', 'bower').lt('1.13.0');
-  //   // if (isOldEmber) {
-  //   //   tree = new Funnel(tree, { exclude: [ /instance-initializers/ ] });
-  //   // }
-  //   var bourbonPath = path.join(this.project.bowerDirectory, 'bourbon', 'app');
-  //   var bourbonTree = new Funnel(this.treeGenerator(bourbonPath), {
-  //     srcDir: '/assets/stylesheets',
-  //     destDir: '/app/styles'
-  //   });
-  //   console.log(this.project.name());
-  //   return bourbonTree;
-  // }
-
+  }
 };
